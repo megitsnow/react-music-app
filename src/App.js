66fff772dd47645454085ecfd858fb4c1d902ea1
megitsnow <meganmarie.nieve@gmail.com>
2023-05-31 14:39:50 -0700

    added playing songs functionality and library functionality

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
-import React from "react";
+import React, {useState} from "react";
+import './styles/app.scss';
 import Song from './components/Song.js'
 import Player from './components/Player.js'
+import Library from './components/Library.js'
+import data from './data.js'
 
 
 function App() {
+
+  const [songs, setSongs] = useState(data());
+  const [currentSong, setCurrentSong] = useState(songs[0]);
+  const [isPlaying, setIsPlaying] = useState(false);
+
   return (
     <div className="App">
-      <Song/>
-      <Player/>
+      <Song currentSong = {currentSong}/>
+      <Player currentSong = {currentSong} isPlaying = {isPlaying} setIsPlaying = {setIsPlaying}/>
+      <Library songs = {songs}/>
     </div>
   );
 }
---
+++ src/components/LibrarySong.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const LibrarySong = ({song}) => {
+    return (
+        <div className = "library-song">
+            <img src = {song.cover} alt = {song.name}/>
+            <div className="song-description">
+                <h2>{song.name}</h2>
+                <h3>{song.artist}</h3>
+            </div>
+        </div>
+    );
+}
+
+export default LibrarySong;
